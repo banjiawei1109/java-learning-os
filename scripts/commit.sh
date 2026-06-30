a8c734282ee448e84b16be21e0afd9bf99028fdf
Ban <banjiawei1109@gmail.com>
2026-06-30 17:16:51 +0800

    docs: 初始化 Java Learning OS

---
+++ scripts/commit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /home/ubuntu/java-learning-os
+
+git add .
+
+git commit -m "$1"
+
+git push
---
+++ scripts/sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+cd /home/ubuntu/java-learning-os
+
+echo "========== Updating Learning OS =========="
+
+git pull
+
+echo "========== Done =========="
